4df24afa606bde46ec5dda7d8429694a426e703d
Nikolay <n.gubenko@kontur2706.ru>
2017-02-13 20:02:54 +0300

    complete home work 1

--- public/index.php
+++ public/index.php
@@ -10,12 +10,12 @@ class Product {
     protected $price;
 
     public function __construct($id, $category, $name, $image, $describe, $price) {
-        $this->id = $id;
+        $this->id       = $id;
         $this->category = $category;
-        $this->name = $name;
-        $this->image = $image;
+        $this->name     = $name;
+        $this->image    = $image;
         $this->describe = $describe;
-        $this->price = $price;
+        $this->price    = $price;
     }
 
     public function view() {
@@ -47,9 +47,9 @@ class SeoProduct extends Product {
 
     public function __construct($id, $category, $name, $image, $describe, $price, $h1, $title, $keywords, $description) {
         parent::__construct($id, $category, $name, $image, $describe, $price);
-        $this->h1 = $h1;
-        $this->title = $title;
-        $this->keywords = $keywords;
+        $this->h1          = $h1;
+        $this->title       = $title;
+        $this->keywords    = $keywords;
         $this->description = $description;
     }
 
@@ -90,16 +90,30 @@ echo $product->prepareDescription();
 
 class A {
     public function foo() {
-        static $x = 0;
-        echo ++$x;
+        static $x = 0;       // Создаем свойства для класса A $x со значением по умолчанию равным 0
+        echo ++$x;           // увеличиваем значение $x на единицу и возвращает новое значение $x при каждом вызове метода foo()
     }
 }
-class B extends A {
-}
-$a1 = new A;
-$b1 = new B;
-$a1->foo();
-$b1->foo();
-$a1->foo();
-$b1->foo();
+$a1 = new A();
+$a2 = new A();
+$a1->foo();                  // Первый вызов метода foo() в результате получаем занчение $x = 1
+$a2->foo();                  // Второй вызов метода foo() в результате получаем занчение $x = 2
+$a1->foo();                  // Третий вызов метода foo() в результате получаем занчение $x = 3
+$a2->foo();                  // Четвертый вызов метода foo() в результате получаем занчение $x = 4
 
+
+
+class A {
+    public function foo() {
+        static $x = 0;       // Создаем свойства для класса A $x со значением по умолчанию равным 0
+        echo ++$x;           // увеличиваем значение $x на единицу и возвращает новое значение $x при каждом вызове метода foo()
+    }
+}
+class B extends A {          // Создаем класс B, который является наследником класса A и в котором будет создано свойство для класса B $x со значением по умолчанию равным 0
+}
+$a1 = new A();
+$b1 = new B();
+$a1->foo();                  // Первый вызов метода foo() класса А в результате получаем занчение $x = 1
+$b1->foo();                  // Первый вызов метода foo() класса B в результате получаем занчение $x = 1
+$a1->foo();                  // Второй вызов метода foo() класса А в результате получаем занчение $x = 2
+$b1->foo();                  // Второй вызов метода foo() класса B в результате получаем занчение $x = 2
